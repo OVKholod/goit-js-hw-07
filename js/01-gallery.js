387c3230b92329053d26e07eb2a001b1c4833310
Oleksandr Kholod <avxolod@gmail.com>
2022-08-16 00:41:39 +0300

    done task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  galleryEl: document.querySelector(".gallery"),
+  galleryList: document.createElement("ul"),
+};
+
+refs.galleryEl.append(refs.galleryList);
+
+loadGallery();
+
+function loadGallery() {
+  let result = galleryItems
+    .map((gItem) => {
+      return `
+      <li class="gallery__item">
+      <img src="${gItem.preview}" alt="${gItem.description}">
+    </li>`;
+    })
+    .join("");
+
+  refs.galleryList.innerHTML = result;
+}
