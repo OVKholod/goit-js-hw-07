a92895c415a3ccbe883a7f6e3493d3673679588e
Oleksandr Kholod <avxolod@gmail.com>
2022-08-18 00:30:59 +0300

    done 7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,22 +5,38 @@ console.log(galleryItems);
 
 const refs = {
   galleryEl: document.querySelector(".gallery"),
-  galleryList: document.createElement("ul"),
 };
-
-refs.galleryEl.append(refs.galleryList);
-
-loadGallery();
 
 function loadGallery() {
   let result = galleryItems
     .map((gItem) => {
       return `
-      <li class="gallery__item">
-      <img src="${gItem.preview}" alt="${gItem.description}">
-    </li>`;
+    <div class="gallery__item">
+    <a class="gallery__link" href="${gItem.original}">
+    <img class="gallery__image" src="${gItem.preview}" alt="${gItem.description}" data-source="${gItem.original}" />
+  </a>
+</div>
+`;
     })
     .join("");
 
-  refs.galleryList.innerHTML = result;
+  refs.galleryEl.insertAdjacentHTML("beforeend", result);
+}
+
+loadGallery();
+
+refs.galleryEl.addEventListener("click", onPictureClick);
+
+function onPictureClick(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains("gallery")) {
+    return;
+  }
+
+  const source = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+`);
+  instance.show();
 }
