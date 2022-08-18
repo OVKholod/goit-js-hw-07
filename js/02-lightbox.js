7eaad99e442916880604fc690079d7c19581ab7a
Oleksandr Kholod <avxolod@gmail.com>
2022-08-19 00:42:20 +0300

    done task 7-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function loadGallery() {
+  let result = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `;
+    })
+    .join("");
+
+  //   galleryList.innerHTML = result;
+  galleryList.insertAdjacentHTML("beforeend", result);
+}
+
+loadGallery();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
